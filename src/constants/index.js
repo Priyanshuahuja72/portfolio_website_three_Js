8c3520d243d9fc642f6c8be07defa1b3b91cb421
Atishaysonii11 <atishaysoniiii11@gmail.com>
2024-09-13 15:46:34 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,205 @@
+export const navLinks = [
+    {
+      id: 1,
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      id: 2,
+      name: 'About',
+      href: '#about',
+    },
+    {
+      id: 3,
+      name: 'Work',
+      href: '#work',
+    },
+    {
+      id: 4,
+      name: 'Contact',
+      href: '#contact',
+    },
+  ];
+  
+  export const clientReviews = [
+    {
+      id: 1,
+      name: 'Emily Johnson',
+      position: 'Marketing Director at GreenLeaf',
+      img: 'assets/review1.png',
+      review:
+        'Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.',
+    },
+    {
+      id: 2,
+      name: 'Mark Rogers',
+      position: 'Founder of TechGear Shop',
+      img: 'assets/review2.png',
+      review:
+        'Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional! Fantastic work.',
+    },
+    {
+      id: 3,
+      name: 'John Dohsas',
+      position: 'Project Manager at UrbanTech ',
+      img: 'assets/review3.png',
+      review:
+        'I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.',
+    },
+    {
+      id: 4,
+      name: 'Ether Smith',
+      position: 'CEO of BrightStar Enterprises',
+      img: 'assets/review4.png',
+      review:
+        'Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend backend dev are top-notch.',
+    },
+  ];
+  
+  export const myProjects = [
+    {
+      title: 'NFT-Marketplace- landing Page',
+      desc: 'An innovative NFT HomePage designed to get best user intraction. It is designed interactive landing page UI with HTML, CSS, and JavaScript. Developed custom animations with JavaScript and CSS to enhance dynamic visual effects.',
+      subdesc:
+        'With a focus on efficiency, It integrated complex animations by using advance JavaScript',
+      href: 'https://priyanshuahuja72.github.io/youtube-first-project-one/',
+      texture: '/textures/project/project1.mp4',
+      logo: '/assets/project-logo1.png',
+      logoStyle: {
+        backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+      },
+      spotlight: '/assets/spotlight1.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'Yoom-meeting-App',
+      desc: 'An innovative Meeting-App to get best user inetraction.Implemented Clerk and Stream for authentication and real-time video chat features, incorporating meeting room creation functionality.',
+      subdesc:
+        'With a focus on efficiency, Yoom integrantes complex Meeting and Authentication, by using Next.js, clerk, and Stripe that enhance operational workflows.',
+      href: 'https://yoom-meeting-app.vercel.app/',
+      texture: '/textures/project/project2.mp4',
+      logo: '/assets/project-logo2.png',
+      logoStyle: {
+        backgroundColor: '#13202F',
+        border: '0.2px solid #17293E',
+        boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      },
+      spotlight: '/assets/spotlight2.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+    {
+      title: 'CarePulse - Health Management System',
+      desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+      subdesc:
+        'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
+      href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+      texture: '/textures/project/project3.mp4',
+      logo: '/assets/project-logo3.png',
+      logoStyle: {
+        backgroundColor: '#60f5a1',
+        background:
+          'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+        border: '0.2px solid rgba(208, 213, 221, 1)',
+        boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      },
+      spotlight: '/assets/spotlight3.png',
+      tags: [
+        {
+          id: 1,
+          name: 'React.js',
+          path: '/assets/react.svg',
+        },
+        {
+          id: 2,
+          name: 'TailwindCSS',
+          path: 'assets/tailwindcss.png',
+        },
+        {
+          id: 3,
+          name: 'TypeScript',
+          path: '/assets/typescript.png',
+        },
+        {
+          id: 4,
+          name: 'Framer Motion',
+          path: '/assets/framer.png',
+        },
+      ],
+    },
+  ];
+  
+  export const calculateSizes = (isSmall, isMobile, isTablet) => {
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
+  };
+  
+  export const workExperiences = [
+    {
+      id: 1,
+      name: 'Glance/InMobi',
+      pos: 'Frontend Developer',
+      duration: '2023 - 2024',
+      title: "Glance is a smart lock screen experience that helps you discover everything you love, as it happens, in just a simple glance. I worked in the Glance Shop101 Panel where I have designed that panel.",
+      icon: '/assets/glance.svg',
+      animation: 'victory',
+    },
+    {
+      id: 2,
+      name: 'SimplePlan Media',
+      pos: 'Junior Full Stack web Developer',
+      duration: '2019 - 2020',
+      title: "SimplePlan Media is a startup where i worked as a full stack Junior web developer. I worked in many big projects like JoshTalks website, Paddy Compare etc.",
+      icon: '/assets/SimplePlan.svg',
+      animation: 'salute',
+    },
+  ];
